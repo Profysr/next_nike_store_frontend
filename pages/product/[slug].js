24377840223c3e20412ddaed33d98385c2151df0
unknown <m.profysrbilal.pk76@gmail.com>
2023-06-06 00:37:38 -0700

    minor changes in  product slug

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,6 +1,318 @@
-import React, { useState } from "react";
-import { IoMdHeart, IoMdHeartEmpty } from "react-icons/io";
+// import React, { useState } from "react";
+// import { IoMdHeart, IoMdHeartEmpty } from "react-icons/io";
 
+// import Wrapper from "@/components/Wrapper";
+// import ProductDetailsCarousel from "@/components/ProductDetailsCarousel";
+// import RelatedProducts from "@/components/RelatedProducts";
+// import { fetchDataFromApi } from "@/utils/api";
+// import { discountPercentage } from "@/utils/helper";
+// import ReactMarkdown from "react-markdown";
+// import { useSelector, useDispatch } from "react-redux";
+// import { addToCart } from "@/store/cartSlice";
+// import { NumericFormat } from "react-number-format";
+
+// import { ToastContainer, toast } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// import { addToFavourite } from "@/store/wishSlice";
+
+// const ProductDetails = ({ product, products }) => {
+//   const [selectedSize, setSelectedSize] = useState();
+//   const [showError, setShowError] = useState(false);
+//   const dispatch = useDispatch();
+//   const p = product?.data?.[0]?.attributes;
+
+//   const notify = () => {
+//     toast.success("Success. Check your cart!", {
+//       position: "bottom-right",
+//       autoClose: 5000,
+//       hideProgressBar: false,
+//       closeOnClick: true,
+//       pauseOnHover: true,
+//       draggable: true,
+//       progress: undefined,
+//       theme: "dark",
+//     });
+//   };
+
+//   // WISHLIST ICON STATE
+//   const [wishIcon, setWishIcon] = useState(false);
+//   return (
+//     <div className="w-full md:py-20">
+//       <ToastContainer />
+//       <Wrapper>
+//         <div className="flex flex-col lg:flex-row md:px-10 gap-[50px] lg:gap-[100px]">
+//           {/* left column start */}
+//           <div className="w-full md:w-auto flex-[1.5] max-w-[500px] lg:max-w-full mx-auto lg:mx-0">
+//             <ProductDetailsCarousel images={p.image.data} />
+//           </div>
+//           {/* left column end */}
+
+//           {/* right column start */}
+//           <div id="topView" className="flex-[1] py-3">
+//             {/* PRODUCT TITLE */}
+//             <div className="text-[34px] font-semibold mb-2 leading-tight">
+//               {p.name}
+//             </div>
+
+//             {/* PRODUCT SUBTITLE */}
+//             <div className="text-lg font-semibold mb-5">{p.subtitle}</div>
+
+//             {/* PRODUCT PRICE */}
+//             <div className="flex items-center">
+//               <p className="mr-2 text-lg font-semibold">
+//                 <NumericFormat
+//                   value={p?.price}
+//                   displayType="text"
+//                   thousandSeparator=","
+//                   prefix="Rs: "
+//                 />
+//               </p>
+//               {p.original_price && (
+//                 <>
+//                   <p className="text-base  font-medium line-through">
+//                     <NumericFormat
+//                       value={p.original_price}
+//                       displayType="text"
+//                       thousandSeparator=","
+//                     />
+//                   </p>
+//                   <p className="ml-auto text-base font-medium text-green-500">
+//                     {discountPercentage(p.original_price, p.price)}% off
+//                   </p>
+//                 </>
+//               )}
+//             </div>
+
+//             <div className="text-md font-medium text-black/[0.5]">
+//               incl. of taxes
+//             </div>
+//             <div className="text-md font-medium text-black/[0.5] mb-20">
+//               {`(Also includes all applicable duties)`}
+//             </div>
+
+//             {/* PRODUCT SIZE RANGE START */}
+//             <div className="mb-10">
+//               {/* HEADING START */}
+//               <div className="flex justify-between mb-2">
+//                 <div className="text-md font-semibold">Select Size</div>
+//                 <div className="text-md font-medium text-black/[0.5] cursor-pointer">
+//                   Select Guide
+//                 </div>
+//               </div>
+//               {/* HEADING END */}
+
+//               {/* SIZE START */}
+//               <div id="sizesGrid" className="grid grid-cols-3 gap-2">
+//                 {p.size.data.map((item, i) => (
+//                   <div
+//                     key={i}
+//                     className={`border rounded-md text-center py-3 font-medium ${
+//                       item.enabled
+//                         ? "hover:border-black cursor-pointer"
+//                         : "cursor-not-allowed bg-black/[0.1] opacity-50"
+//                     } ${selectedSize === item.size ? "border-black" : ""}`}
+//                     // onClick={() => {
+//                     //   setSelectedSize(item.size);
+//                     //   setShowError(false);
+//                     // }}
+//                     onClick={() => {
+//                       setSelectedSize(item.size),
+//                         setShowError(false),
+//                         document.getElementById("wishListBtn").scrollIntoView({
+//                           behavior: "smooth",
+//                           block: "end",
+//                         });
+//                     }}
+//                   >
+//                     {item.size}
+//                   </div>
+//                 ))}
+//               </div>
+//               {/* SIZE END */}
+
+//               {/* SHOW ERROR START */}
+//               {showError && (
+//                 <div className="text-red-600 mt-1">
+//                   Size selection is required
+//                 </div>
+//               )}
+//               {/* SHOW ERROR END */}
+//             </div>
+//             {/* PRODUCT SIZE RANGE END */}
+
+//             {/* Add to cart button starts  */}
+//             {/* <button
+//               className="rounded-full w-full py-4 bg-black text-white text-lg font-medium mb-3 transition-transform duration-200 active:scale-95 hover:opacity-75"
+//               onClick={() => {
+//                 if (!selectedSize) {
+//                   setShowError(true);
+//                   document
+//                     .getElementById("sizeGrid")
+//                     .scrollIntoView({ behavior: "smooth", block: "center" });
+//                 } else {
+//                   dispatch(
+//                     addToCart({
+//                       ...singleProduct?.data?.[0],
+//                       selectedSize,
+//                       oneQuantityPrice: p?.price,
+//                     })
+//                   );
+//                   notify();
+//                   document.getElementById("topView").scrollIntoView({
+//                     behavior: "smooth",
+//                     block: "start",
+//                   });
+//                 }
+//               }}
+//             >
+//               Add to Cart
+//             </button> */}
+//             {/* Add to cart button ends  */}
+//             {/* ADD TO CART BUTTON START */}
+//             {selectedSize ? (
+//               <button
+//                 className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75"
+//                 onClick={() => {
+//                   if (!selectedSize) {
+//                     setShowError(true);
+//                     document.getElementById("sizesGrid").scrollIntoView({
+//                       block: "center",
+//                       behavior: "smooth",
+//                     });
+//                   } else {
+//                     dispatch(
+//                       addToCart({
+//                         ...product?.data?.[0],
+//                         selectedSize,
+//                         oneQuantityPrice: p.price,
+//                       })
+//                     );
+//                     document.getElementById("topView").scrollIntoView({
+//                       behavior: "smooth",
+//                       block: "start",
+//                     });
+
+//                     notify();
+//                   }
+//                 }}
+//               >
+//                 Add to Cart
+//               </button>
+//             ) : (
+//               <button
+//                 className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75"
+//                 onClick={() => {
+//                   if (!selectedSize) {
+//                     setShowError(true);
+//                     document.getElementById("sizesGrid").scrollIntoView({
+//                       block: "center",
+//                       behavior: "smooth",
+//                     });
+//                   } else {
+//                     dispatch(
+//                       addToCart({
+//                         ...product?.data?.[0],
+//                         selectedSize,
+//                         oneQuantityPrice: p.price,
+//                       })
+//                     );
+//                     document.getElementById("topView").scrollIntoView({
+//                       behavior: "smooth",
+//                       block: "start",
+//                     });
+//                     notify();
+//                   }
+//                 }}
+//               >
+//                 Add to Cart
+//               </button>
+//             )}
+//             {/* ADD TO CART BUTTON END */}
+
+//             {/* WHISHLIST BUTTON START */}
+//             <button
+//               className="w-full py-4 rounded-full border border-black text-lg font-medium transition-transform active:scale-95 flex items-center justify-center gap-2 hover:opacity-75 mb-10"
+//               onMouseEnter={() => setWishIcon(true)}
+//               onMouseLeave={() => setWishIcon(false)}
+//               id="wishListBtn"
+//               onClick={() => {
+//                 if (!selectedSize) {
+//                   setShowError(true);
+//                   document.getElementById("sizesGrid").scrollIntoView({
+//                     block: "center",
+//                     behavior: "smooth",
+//                   });
+//                 } else {
+//                   dispatch(
+//                     addToFavourite({
+//                       ...product?.data?.[0],
+//                       selectedSize,
+//                       oneQuantityPrice: p.price,
+//                     })
+//                   );
+//                 }
+//               }}
+//             >
+//               Whishlist
+//               {wishIcon ? (
+//                 <IoMdHeart size={30} />
+//               ) : (
+//                 <IoMdHeartEmpty size={30} />
+//               )}
+//             </button>
+//             {/* WHISHLIST BUTTON END */}
+
+//             <div>
+//               <div className="text-lg font-bold mb-5">Product Details</div>
+//               <div className="markdown text-md mb-5">
+//                 <ReactMarkdown>{p.description}</ReactMarkdown>
+//               </div>
+//             </div>
+//           </div>
+//           {/* right column end */}
+//         </div>
+
+//         <RelatedProducts products={products} />
+//       </Wrapper>
+//     </div>
+//   );
+// };
+
+// export default ProductDetails;
+
+// export async function getStaticPaths() {
+//   const products = await fetchDataFromApi("/api/products?populate=*");
+
+//   const paths = products?.data?.map((p) => ({
+//     params: {
+//       slug: p.attributes.slug,
+//     },
+//   }));
+
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
+
+// export async function getStaticProps({ params: { slug } }) {
+//   const product = await fetchDataFromApi(
+//     `/api/products?populate=*&filters[slug][$eq]=${slug}`
+//   );
+//   const products = await fetchDataFromApi(
+//     `/api/products?populate=*&[filters][slug][$ne]=${slug}`
+//   );
+
+//   return {
+//     props: {
+//       product,
+//       products,
+//     },
+//   };
+// }
+
+import React, { useState } from "react";
+import { IoMdHeartEmpty } from "react-icons/io";
 import Wrapper from "@/components/Wrapper";
 import ProductDetailsCarousel from "@/components/ProductDetailsCarousel";
 import RelatedProducts from "@/components/RelatedProducts";
@@ -13,7 +325,6 @@ import { NumericFormat } from "react-number-format";
 
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { addToFavourite } from "@/store/wishSlice";
 
 const ProductDetails = ({ product, products }) => {
   const [selectedSize, setSelectedSize] = useState();
@@ -34,8 +345,6 @@ const ProductDetails = ({ product, products }) => {
     });
   };
 
-  // WISHLIST ICON STATE
-  const [wishIcon, setWishIcon] = useState(false);
   return (
     <div className="w-full md:py-20">
       <ToastContainer />
@@ -111,17 +420,9 @@ const ProductDetails = ({ product, products }) => {
                         ? "hover:border-black cursor-pointer"
                         : "cursor-not-allowed bg-black/[0.1] opacity-50"
                     } ${selectedSize === item.size ? "border-black" : ""}`}
-                    // onClick={() => {
-                    //   setSelectedSize(item.size);
-                    //   setShowError(false);
-                    // }}
                     onClick={() => {
-                      setSelectedSize(item.size),
-                        setShowError(false),
-                        document.getElementById("wishListBtn").scrollIntoView({
-                          behavior: "smooth",
-                          block: "end",
-                        });
+                      setSelectedSize(item.size);
+                      setShowError(false);
                     }}
                   >
                     {item.size}
@@ -140,101 +441,9 @@ const ProductDetails = ({ product, products }) => {
             </div>
             {/* PRODUCT SIZE RANGE END */}
 
-            {/* Add to cart button starts  */}
-            {/* <button
-              className="rounded-full w-full py-4 bg-black text-white text-lg font-medium mb-3 transition-transform duration-200 active:scale-95 hover:opacity-75"
-              onClick={() => {
-                if (!selectedSize) {
-                  setShowError(true);
-                  document
-                    .getElementById("sizeGrid")
-                    .scrollIntoView({ behavior: "smooth", block: "center" });
-                } else {
-                  dispatch(
-                    addToCart({
-                      ...singleProduct?.data?.[0],
-                      selectedSize,
-                      oneQuantityPrice: p?.price,
-                    })
-                  );
-                  notify();
-                  document.getElementById("topView").scrollIntoView({
-                    behavior: "smooth",
-                    block: "start",
-                  });
-                }
-              }}
-            >
-              Add to Cart
-            </button> */}
-            {/* Add to cart button ends  */}
             {/* ADD TO CART BUTTON START */}
-            {selectedSize ? (
-              <button
-                className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75"
-                onClick={() => {
-                  if (!selectedSize) {
-                    setShowError(true);
-                    document.getElementById("sizesGrid").scrollIntoView({
-                      block: "center",
-                      behavior: "smooth",
-                    });
-                  } else {
-                    dispatch(
-                      addToCart({
-                        ...product?.data?.[0],
-                        selectedSize,
-                        oneQuantityPrice: p.price,
-                      })
-                    );
-                    document.getElementById("topView").scrollIntoView({
-                      behavior: "smooth",
-                      block: "center",
-                    });
-
-                    notify();
-                  }
-                }}
-              >
-                Add to Cart
-              </button>
-            ) : (
-              <button
-                className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75"
-                onClick={() => {
-                  if (!selectedSize) {
-                    setShowError(true);
-                    document.getElementById("sizesGrid").scrollIntoView({
-                      block: "center",
-                      behavior: "smooth",
-                    });
-                  } else {
-                    dispatch(
-                      addToCart({
-                        ...product?.data?.[0],
-                        selectedSize,
-                        oneQuantityPrice: p.price,
-                      })
-                    );
-                    document.getElementById("topView").scrollIntoView({
-                      behavior: "smooth",
-                      block: "center",
-                    });
-                    notify();
-                  }
-                }}
-              >
-                Add to Cart
-              </button>
-            )}
-            {/* ADD TO CART BUTTON END */}
-
-            {/* WHISHLIST BUTTON START */}
             <button
-              className="w-full py-4 rounded-full border border-black text-lg font-medium transition-transform active:scale-95 flex items-center justify-center gap-2 hover:opacity-75 mb-10"
-              onMouseEnter={() => setWishIcon(true)}
-              onMouseLeave={() => setWishIcon(false)}
-              id="wishListBtn"
+              className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75"
               onClick={() => {
                 if (!selectedSize) {
                   setShowError(true);
@@ -244,21 +453,28 @@ const ProductDetails = ({ product, products }) => {
                   });
                 } else {
                   dispatch(
-                    addToFavourite({
+                    addToCart({
                       ...product?.data?.[0],
                       selectedSize,
                       oneQuantityPrice: p.price,
                     })
                   );
+                  document.getElementById("topView").scrollIntoView({
+                    block: "start",
+                    behavior: "smooth",
+                  });
+                  notify();
                 }
               }}
             >
+              Add to Cart
+            </button>
+            {/* ADD TO CART BUTTON END */}
+
+            {/* WHISHLIST BUTTON START */}
+            <button className="w-full py-4 rounded-full border border-black text-lg font-medium transition-transform active:scale-95 flex items-center justify-center gap-2 hover:opacity-75 mb-10">
               Whishlist
-              {wishIcon ? (
-                <IoMdHeart size={30} />
-              ) : (
-                <IoMdHeartEmpty size={30} />
-              )}
+              <IoMdHeartEmpty size={20} />
             </button>
             {/* WHISHLIST BUTTON END */}
 
@@ -282,7 +498,6 @@ export default ProductDetails;
 
 export async function getStaticPaths() {
   const products = await fetchDataFromApi("/api/products?populate=*");
-
   const paths = products?.data?.map((p) => ({
     params: {
       slug: p.attributes.slug,
